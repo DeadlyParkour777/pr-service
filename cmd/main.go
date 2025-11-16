07dae71526be41b74c0eeed84e2bb8f4295f5864
deadlyparkour <deadlyparkour@gmail.com>
2025-11-16 17:17:13 +0300

    main and config

---
+++ cmd/main.go
@@ -0,0 +1,74 @@
+package main
+
+import (
+	"context"
+	"log"
+	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
+
+	"github.com/DeadlyParkour777/pr-service/internal/config"
+	"github.com/DeadlyParkour777/pr-service/internal/handler"
+	"github.com/DeadlyParkour777/pr-service/internal/service"
+	"github.com/DeadlyParkour777/pr-service/internal/store"
+)
+
+func main() {
+	if err := run(); err != nil {
+		log.Fatal(err)
+	}
+}
+
+func run() error {
+	cfg, err := config.NewConfig()
+	if err != nil {
+		return err
+	}
+
+	store, err := store.NewStore(cfg.DatabaseURL)
+	if err != nil {
+		return err
+	}
+	defer store.Close()
+
+	deps := service.Dependencies{
+		TeamRepo: store.Team(),
+		UserRepo: store.User(),
+		PRRepo:   store.PR(),
+	}
+
+	service := service.NewService(deps)
+	handler := handler.NewHandler(service)
+	router := handler.InitRoutes()
+
+	server := &http.Server{
+		Addr:    ":" + cfg.HTTP_PORT,
+		Handler: router,
+	}
+
+	stop := make(chan os.Signal, 1)
+	signal.Notify(stop, syscall.SIGTERM, syscall.SIGINT)
+
+	go func() {
+		log.Printf("Server started at port %s", cfg.HTTP_PORT)
+		if err := server.ListenAndServe(); err != nil && err != http.ErrServerClosed {
+			log.Fatalf("Failed to start server: %v, ", err)
+		}
+	}()
+
+	<-stop
+
+	log.Println("Shutting down")
+
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
+	if err := server.Shutdown(ctx); err != nil {
+		return err
+	}
+
+	log.Println("Server stopped")
+	return nil
+}
---
+++ internal/config/config.go
@@ -0,0 +1,28 @@
+package config
+
+import (
+	"fmt"
+	"os"
+)
+
+type Config struct {
+	HTTP_PORT   string
+	DatabaseURL string
+}
+
+func NewConfig() (*Config, error) {
+	port := os.Getenv("HTTP_PORT")
+	if port == "" {
+		port = "8080"
+	}
+
+	dbURL := os.Getenv("DATABASE_URL")
+	if dbURL == "" {
+		return nil, fmt.Errorf("DATABASE URL environment variable is not set")
+	}
+
+	return &Config{
+		HTTP_PORT:   port,
+		DatabaseURL: dbURL,
+	}, nil
+}
